580cae7a71928d43eb677a2305a552d4e83aae5e
Pius <piuskri@gmail.com>
2023-10-31 15:59:03 +0100

    add runner output

--- src/slurm_runner.sh
+++ src/slurm_runner.sh
@@ -9,6 +9,13 @@ mkdir -p ${DIRECTORY}/jobs
 # Exit on errors
 set -o errexit
 
+# Send some noteworthy information to the output log
+echo "=== Start slurm scipt ==="
+echo "Running on node: $(hostname)"
+echo "In directory:    $(pwd)"
+echo "Starting on:     $(date)"
+echo "SLURM_JOB_ID:    ${SLURM_JOB_ID}\n\n"
+
 # Set a directory for temporary files unique to the job with automatic removal at job termination
 TMPDIR=$(mktemp -d)
 if [[ ! -d ${TMPDIR} ]]; then
@@ -22,21 +29,16 @@ export TMPDIR
 # Change the current directory to the location where you want to store temporary files, exit if changing didn't succeed.
 # Adapt this to your personal preference
 cd "${TMPDIR}" || exit 1
-
-# Send some noteworthy information to the output log
-echo "Running on node: $(hostname)"
-echo "In directory:    $(pwd)"
-echo "Starting on:     $(date)"
-echo "SLURM_JOB_ID:    ${SLURM_JOB_ID}"
+echo "-> create and set tmp directory ${TMPDIR}"
 
 # activate conda
 [[ -f /itet-stor/${ETH_USERNAME}/net_scratch/conda/bin/conda ]] && eval "$(/itet-stor/${ETH_USERNAME}/net_scratch/conda/bin/conda shell.bash hook)"
 conda activate ${CONDA_ENVIRONMENT}
-echo "Conda activated"
+echo "-> conda_env ${CONDA_ENVIRONMENT} activated"
 cd ${DIRECTORY}
 
 # Binary or script to execute
-echo "run train.py from directory $(pwd)"
+echo "->run train.py from directory $(pwd)"
 python /itet-stor/kpius/net_scratch/si_bees/src/train.py
 
 # Send more noteworthy information to the output log
