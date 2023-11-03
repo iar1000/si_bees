fa3e517b3d086c67b41a96d25dbb9b6db1537bcc
Pius <piuskri@gmail.com>
2023-11-03 18:05:18 +0100

    debug

--- src/envs/communication_v0/curriculum.py
+++ src/envs/communication_v0/curriculum.py
@@ -4,7 +4,8 @@ def curriculum_fn(train_results, task_settable_env, env_ctx):
     """expand the distance between the oracle and the plattform if agents have a positive reward"""
     current_task = task_settable_env.get_task()
     reward = train_results["episode_reward_mean"]
-    print(train_results["custom_metrics"])
+    print("HEEEEEELLOO", train_results["custom_metrics"])
+    quit()
     optimality = train_results["custom_metrics"]["episode_performance_per_100_mean"]
     if optimality > 0.8:
         return current_task + 1
