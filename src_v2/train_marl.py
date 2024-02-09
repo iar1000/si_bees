1c01b75899b0e832e5e5ba2da5df49b7c4efd06d
Pius <piuskri@gmail.com>
2024-02-09 18:40:55 +0100

    debug

--- src_v2/train_marl.py
+++ src_v2/train_marl.py
@@ -138,7 +138,7 @@ if __name__ == '__main__':
             batch_mode="complete_episodes")
         ppo_config.resources(
                 num_gpus=1.0 / int(args.num_gpu_shares),
-                custom_resources_per_worker={"GPU": 0, "CPU": 1},
+                custom_resources_per_worker={"num_gpus": 0, "num_cpus": 1},
                 
                 # num_learner_workers=0,
                 # num_gpus_per_learner_worker=1,
