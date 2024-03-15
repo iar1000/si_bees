c3b70d2efcce9c5364c35ad208dd7ea8c9f96708
Pius <piuskri@gmail.com>
2024-03-15 19:38:01 +0100

    asfd

--- src_v3/gnn.py
+++ src_v3/gnn.py
@@ -203,7 +203,7 @@ class gnn_torch_module(TorchModelV2, Module):
             x, actor_edge_index, actor_edge_attr, fc_edge_index, fc_edge_attr = get_graph_from_batch_obs(self.num_agents, agent_obss, edge_obss, i) 
 
             for e in x:
-                print(type(e))
+                print(e.get_device())
 
             # encode node and edge states
             x_old = torch.clone(torch.stack([v for v in x]))
