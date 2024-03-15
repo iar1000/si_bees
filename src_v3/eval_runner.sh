adfa501281218c00f9c29b7c9f5c66a86ee6185a
Pius <piuskri@gmail.com>
2024-03-15 10:06:49 +0100

    basic eval runner

---
+++ src_v3/eval_runner.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+ETH_USERNAME=kpius
+PROJECT_NAME=si_bees
+SRC_DIR="src_v3"
+PROJECT_DIR=/itet-stor/${ETH_USERNAME}/net_scratch/${PROJECT_NAME}
+TMP_DIR=/itet-stor/${ETH_USERNAME}/net_scratch
+CONDA_BIN=/itet-stor/${ETH_USERNAME}/net_scratch/conda/bin/conda
+CONDA_ENVIRONMENT=swarm
+
+mkdir -p ${PROJECT_DIR}/jobs
+
+# Exit on errors
+set -o errexit
+
+# activate conda
+[[ -f $CONDA_BIN ]] && eval "$($CONDA_BIN shell.bash hook)"
+conda activate ${CONDA_ENVIRONMENT}
+echo "-> conda_env ${CONDA_ENVIRONMENT} activated"
+cd ${PROJECT_DIR}
+
+
+# Send some noteworthy information to the output log
+echo ""
+echo "=== Start slurm scipt ==="
+echo "Running on node: $(hostname)"
+echo "In directory:    $(pwd)"
+echo "Starting on:     $(date)"
+echo "SLURM_JOB_ID:    ${SLURM_JOB_ID}"
+
+# execute script
+echo "-> current directory $(pwd)"
+echo "-> run eval.py from directory "
+python $PROJECT_DIR/xxx_mpe.py
+
+# Send more noteworthy information to the output log
+echo "Finished at:     $(date)"
+
+# End the script with exit code 0
+exit 0
+
